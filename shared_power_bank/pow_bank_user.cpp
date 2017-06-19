88305d1df48c9a8a933df5b2a9733f516ea1ba05
xd <xd@DESKTOP-0SGD71G>
2017-06-19 21:57:19 +0800

    修改loction为location

--- shared_power_bank/pow_bank_user.cpp
+++ shared_power_bank/pow_bank_user.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 #include "pow_bank_user.h"
-
+#include "location_service.h"
 using namespace std;
 
 void PowBankUser::GetInfo(string name, string phone_number) {
@@ -9,7 +9,7 @@ void PowBankUser::GetInfo(string name, string phone_number) {
 	user_phone_number = phone_number;
 }
 void PowBankUser::GetLocation() {
-
+	current_location = LocService::GetLocation();
 }
 void PowBankUser::ChoosePowDep() {
 
