33a5fe1ef50120798938d455d8c9de46c43e5f13
xd <xd@DESKTOP-0SGD71G>
2017-06-19 20:45:20 +0800

    完善了注释和用户类

--- shared_power_bank/pow_bank_user.h
+++ shared_power_bank/pow_bank_user.h
@@ -8,18 +8,22 @@ using namespace std;
 class PowUser {
 public:
 	PowUser() = default;
-	void get_info(string s);		//从数据库获取用户信息
-	void create_order();		//形成订单
-	void get_location();		//得到当前位置
+	void get_info(string s);			//从数据库获取用户信息
+	void create_order();				//形成订单
+	void get_location();				//得到当前位置
+	void choose_pow_dep();				//选择存放机
+	void get_chk_code();				//从存放机管理系统得到验证码
+	int show_chk_code();				//返回验证码给借出存放机
+
 
 private:
 	string user_name;
 	string user_phone_number;
 	pair<float, float> current_location;
-	string chk_code;     //用户得到的验证码
-	float balance;		//用户余额
-	//bool get_pow;		//判断用户是否借了充电宝
-	int pow_deposit_number;		//用户选择的存放机
-	PowBank user_pow;		//用户的
-	PowBankDepositry chosen_dep;	//用户所选择的存放机
+	int chk_code;						//用户得到的验证码
+	float balance;						//用户余额
+	//bool get_pow;						//判断用户是否借了充电宝
+	int pow_deposit_number;				//用户选择的存放机
+	PowBank user_pow;					//用户所借的充电宝
+	PowBankDepository chosen_dep;		//用户所选择的存放机
 };
