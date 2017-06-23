b0c28b0aeeaf06e25caaa1c7c49a8556406cea0b
xd <xd@DESKTOP-N9KB84F>
2017-06-23 23:44:40 +0800

    完成main函数

--- shared_power_bank/main.cpp
+++ shared_power_bank/main.cpp
@@ -9,116 +9,9 @@
 int optcode;
 using namespace std;
 int main() {
-
-	
-	/*
-	PowBankUser user;
-	cout << "欢迎使用XX共享充电宝" << endl;
-	cout << "[0] 退出程序		[1] 登陆			[2] 注册" << endl;
-	re_input1:
-	cin >> optcode;
-	string name;
-	string number;
-	switch (optcode) {
-	case 0:
-		return 0;
-	case 1:
-		//get
-		break;
-	case 2:
-		cout << "请输入您的名字和电话号码，中间用回车分隔" << endl;
-		cout << "请输入您的名字：" ;
-		cin >> name ;
-		cout << "请输入您的电话号码：" ;
-		cin >> number;
-		user.GetInfo(name, number);
-		break;
-	default:
-		cout << "对不起，您输入有误请重新输入" << endl;
-		goto re_input1;
-	}
-	if (optcode == 1)
-		cout << "您已成功登陆！" << endl;
-	if (optcode == 2)
-	{
-		cout << "您已注册成功！" << endl;
-		cout << "需要立即登陆吗？			[1] 不需要 [2] 需要" << endl;
-		cin >> optcode;
-		if (optcode == 2)
-			goto borrow;       
-		else
-			return 0;
-	}
-	cout << "请问您接下来想执行什么操作？" << endl;
-
-	borrow:     //在这个结点，可以用来借充电宝
-
-	cout << "[0] 退出程序			[1] 查询附近的充电宝" << endl;
-	re_input2:
-	cin >> optcode;
-	switch (optcode) {
-	case 0:          
-		return 0;
-	case 1:
-		user.GetLocation();
-		user.ChoosePowDep();
-		break;
-	default:
-		cout << "对不起，您输入有误请重新输入" << endl;
-		goto re_input2;
-	}
-	if (optcode == 1)
-	{
-		cout << "查询附近充电宝成功，请问您接下来需要什么做什么？" << endl;
-		cout << "[0] 退出程序			[1] 借充电宝" << endl;
-	}
-	cin >> optcode;
-	if (optcode != 1)
-		return 0;
-	if (optcode == 1) {
-		cout << "您的验证码是：";//<<user.ShowChkCode()<< endl;
-	}
-	int input_times = 0;
-	while (true) {				 
-		//接受http，判断验证码是否相等
-		//在此处进行判断
-		cout << "您输入的验证码有误，请重新输入" << endl;
-		input_times++;
-		if (input_times == 3) {
-			cout << "您已输错3次，请待会重试" << endl;
-			return 0;
-		}
-	}
-	cout << "请在机器下方取出充电宝，祝你使用愉快！" << endl;
-	float costs;				 //接触充电宝总花费
-	time_t begin_time;			 //计时
-	cout << "欢迎回到主界面，你可以执行以下操作：" <<endl<< 
-		"[0] 退出程序	   [1]借一个充电宝		[2]归还充电宝"<<endl;
-	cin >> optcode;
-	switch (optcode) {
-	case 0:
-		return 0;
-	case 1:
-		if (!user.IfGetPow()) {
-			user.GetLocation();
-			user.ChoosePowDep();
-			break;
-		}
-		else {
-			cout << "您已经借了一个充电宝了，请在归还充电包之后在执行操作" << endl;
-			break;
-		}
-	case 2:
-		user.ReturnPow();
-		cout << "你已成功归还充电宝，欢迎下次使用！" << endl;
-	default:
-		;
-	}
-	*/
 	//新版本
 	PowBankUser user;
 	string name, number,password;		//用户要存储的姓名和电话
-	time_t begin, end;
 	vector<vector<string>> interface;
 	cout << "欢迎来到XX共享充电宝！！！" << endl;
 	//cout << "本程序包含一下界面" << endl;
@@ -250,8 +143,13 @@ repositry:
 			cout << "您还没有选择一个存放机" << endl;
 			cout << "是否要选择一个存放机? 0 or 1" << endl;
 			cin >> optcode;
-
+			if (optcode == 0) {
+				goto repositry;
+			}
+			if (optcode == 1)
+				goto borrowandlend;
 		}
+		break;
 	default:
 		return 0;
 	}
@@ -270,16 +168,11 @@ borrowandlend:
 	case 1:
 		user.ShowChkCode();
 		user.BorrowPow();
-		time_t t1;
-		time(&t1);
-		begin = t1;
+		break;
 case22:
 	case 2:
 		if (user.IfGetPow()) {
 			user.ReturnPow();
-			time_t t2;
-			time(&t2);
-			end = t2;
 		}
 		else {
 			cout << "您还没有借充电宝，不需要归还" << endl;
@@ -290,8 +183,10 @@ case22:
 			if (optcode == 1)
 				goto case22;
 		}
+		break;
 	case 3:
 		user.ShowChkCode();
+		break;
 	case 4:
 		goto repositry;
 	case 5:
@@ -315,8 +210,10 @@ case22:
 		return 0;
 	case 1:
 		cout << "你本次消费的金额为：" <<user.CalculateCost(begin,end)<< endl;
+		break;
 	case 2:
 		user.ShowMoney();
+		break;
 	case 3:
 		goto user_;
 	default:
@@ -324,21 +221,8 @@ case22:
 
 	}
 
-
-
-
-
-
-
-
-
-
-
-
 	getchar();
 	getchar();
-
-
 
 	return 0;
 }
